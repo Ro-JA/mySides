f6d0ff28fec9c317f365d81445a85767db65e38b
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-14 20:29:01 +0900

    refatoring and add color

--- MySides.kt
+++ MySides.kt
@@ -1,15 +1,11 @@
 fun main() {
-    val mySides = Sides();
-
-    val diceRoll = mySides.roll();
-    println("Your ${mySides.sides} sides dice rolled $diceRoll ");
+    val myFirstSides = Sides(6, "red");
+    println("Your ${myFirstSides.numSides} sides dice ${myFirstSides.colorSides} color rolled ${myFirstSides.roll()}!");
+    val mySecondSides = Sides(20, "green");
+    println("Your ${mySecondSides.numSides} sides dice ${mySecondSides.colorSides} color rolled ${mySecondSides.roll()}!");
 
 }
 
-class Sides() {
-    var sides = 6;
-    fun roll(): Int {
-        val numberRandom = (1..6).random();
-        return numberRandom;
-    }
+class Sides(val numSides: Int, val colorSides: String) {
+    fun roll() = (1..numSides).random();
 }
