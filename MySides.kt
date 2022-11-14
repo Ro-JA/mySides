b6b41e4fc9a1dd2a1b107a32d578526a94f3ed4b
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-14 18:43:19 +0900

    correct add return int

--- MySides.kt
+++ MySides.kt
@@ -1,13 +1,15 @@
 fun main() {
     val mySides = Sides();
-    println(mySides.sides);
-    mySides.roll();
+
+    val diceRoll = mySides.roll();
+    println("Your ${mySides.sides} sides dice rolled $diceRoll ");
+
 }
 
 class Sides() {
     var sides = 6;
-    fun roll() {
+    fun roll(): Int {
         val numberRandom = (1..6).random();
-        prinrln(numberRandom);
+        return numberRandom;
     }
 }
