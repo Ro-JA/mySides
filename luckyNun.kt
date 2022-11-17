2a570a1f588b69ceda3556670cf59f42a6594e3b
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-17 10:58:43 +0900

    kotlin if

---
+++ luckyNun.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val num = 4;
+    if (num > 4) {
+        println("The varaibel is greater than 4")
+    } else if (num == 4) {
+        println("The variabel is equal to 4")
+    } else {
+        println("The variabel is less to 4")
+    }
+}
