3a7d352a2856c7a9da2b83f9fc0d90a01ee6d3c8
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-17 12:47:11 +0900

    new corect

--- MySides.kt
+++ MySides.kt
@@ -1,11 +1,18 @@
 fun main() {
-    val myFirstSides = Sides(6, "red");
-    println("Your ${myFirstSides.numSides} sides dice ${myFirstSides.colorSides} color rolled ${myFirstSides.roll()}!");
-    val mySecondSides = Sides(20, "green");
-    println("Your ${mySecondSides.numSides} sides dice ${mySecondSides.colorSides} color rolled ${mySecondSides.roll()}!");
+    val myFirstSides = Sides(6);
+    val rollResult = myFirstSides.roll();
+    val luckyNum = 4;
 
+    when (rollResult) {
+        luckyNum -> println("'You win!");
+        1 -> println("So sorry! You rolled a 1. Try again!");
+        2 -> println("Sadly! You rolled a 2. Try again!");
+        3 -> println("Unfortunately, you rolled 3! Try again!");
+        5 -> println("Don't cru! You rolled 5. Try again!");
+        6 -> println("Apologies! You rolled 6. Try again!");
+    }
 }
 
-class Sides(val numSides: Int, val colorSides: String) {
+class Sides(val numSides: Int) {
     fun roll() = (1..numSides).random();
 }
