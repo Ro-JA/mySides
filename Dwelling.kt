718f181762cb4ffcaca45e6501aff7412ad40057
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-23 22:47:25 +0900

    new class RoundHut

--- Dwelling.kt
+++ Dwelling.kt
@@ -1,7 +1,15 @@
 fun main() {
     val squareCabin = SquareCabin(6)
+    val roundHut = RoundHut(3)
 
     with(squareCabin) {
+        println("\nSquare Cabin\n============")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
+    }
+
+    with(roundHut) {
         println("\nSquare Cabin\n============")
         println("Capacity: ${capacity}")
         println("Material: ${buildingMaterial}")
@@ -22,4 +30,9 @@ abstract class Dwelling(private var residents: Int) {
 class SquareCabin(residents: Int) : Dwelling(residents) {
     override val buildingMaterial = "Wood"
     override val capacity = 6
+}
+
+class RoundHut(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Strong"
+    override val capacity = 4
 }
