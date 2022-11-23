bcb1513dc0426f7b97803f6b62f2d6724501d950
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-23 22:39:28 +0900

    new class SquareCabin

--- Dwelling.kt
+++ Dwelling.kt
@@ -1,5 +1,25 @@
 fun main() {
-    abstract class Dwelling() {
-        abstract val buldingMaterial: String
+    val squareCabin = SquareCabin(6)
+
+    with(squareCabin) {
+        println("\nSquare Cabin\n============")
+        println("Capacity: ${capacity}")
+        println("Material: ${buildingMaterial}")
+        println("Has room? ${hasRoom()}")
     }
+}
+
+
+abstract class Dwelling(private var residents: Int) {
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
 }
