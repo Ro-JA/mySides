ce9ab9405a00a85a948f50fb79fb9561d27f2768
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-11-30 15:10:24 +0900

    work of list

--- list.kt
+++ list.kt
@@ -1,4 +1,15 @@
 fun main() {
     val numbers: List<Int> = listOf(1,2,3,4,5,6)
-    println("List:$numbers",)
-}
+    println("List:$numbers")
+    println("Size list: ${numbers.size}")
+//    Access elements of the list
+    println("First elements: ${numbers[0]}")
+    println("Second elements: ${numbers[1]}")
+    println("Last index: ${numbers.size-1}")
+    println("Last elements: ${numbers[numbers.size-1]}")
+    println("Last: ${numbers.last()}")
+    println("First: ${numbers.first()}")
+//    use the contains() method
+    println("Contains 4: ${numbers.contains(4)}")
+    println("Contains 7: ${numbers.contains(7)}")
+ }
