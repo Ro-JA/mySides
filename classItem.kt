039607c2aab7a39fbf0a4e2649878ac4da065769
Roman <74274722+Ro-JA@users.noreply.github.com>
2022-12-02 14:55:54 +0900

    new class Order

--- classItem.kt
+++ classItem.kt
@@ -1,13 +1,58 @@
 open class Item(val name: String, val price: Int)
 
-class Noodles : Item("Noodles", 10)
+class Noodles : Item("Noodles", 10) {
+    override fun toString(): String {
+        return name
+    }
+}
 
-class Vegetablse : Item("Vegetablse", 5)
+class Vegetables(vararg val toppings: String) : Item("Vegetablse", 5) {
+    override fun toString(): String {
+        if(toppings.isEmpty()) {
+            return "$name Shef's Choice"
+        }
+        return name + " " + toppings.joinToString()
+    }
+}
 
-fun main(){
-    val noodles = Noodles()
-    val vegetablse = Vegetablse()
-    println(noodles)
-    println(vegetablse)
+class Order(val orderNumber: Int) {
+    private val itemList = mutableListOf<Item>()
 
+    fun addItem(newItem: Item) {
+        itemList.add(newItem)
+    }
+
+    fun addAll(newItems: List<Item>) {
+        itemList.addAll(newItems)
+    }
+
+    fun print() {
+        println("Order #$orderNumber")
+        var total = 0
+        for(item in itemList) {
+            println("$item: $${item.price}")
+            total +=item.price
+        }
+        println("Total: $$total")
+
+    }
+}
+fun main() {
+    val order1 = Order(1)
+    order1.addItem(Noodles())
+    order1.print()
+
+    println()
+
+    val order2 = Order(2)
+    order2.addItem(Noodles())
+    order2.addItem(Vegetables())
+    order2.print()
+
+    println()
+
+    val order3 = Order(3)
+    val items = listOf(Noodles(), Vegetables("Carrots", "Beans", "Celery"))
+    order3.addAll(items)
+    order3.print()
 }
